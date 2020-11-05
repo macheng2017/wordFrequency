558fe65adf0a6321b9ce0931d6656f75fd0f0b1c
macheng2017 <gos360@163.com>
2020-11-05 22:39:59 +0800

    feat: 使用map[string]int 实现词频统计，使用struct 以及实现sort接口实现对map的value的排序

--- main/main.go
+++ main/main.go
@@ -7,89 +7,68 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
-	"strings"
+	"sort"
 )
 
-func readFile(path string) {
+func ReadFile(path string) (bytes []byte, err error) {
 	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 	reader := bufio.NewReader(file)
+	bytes, err = ioutil.ReadAll(reader)
+	return bytes, err
+}
 
-	bytes, err := ioutil.ReadAll(reader)
-	fmt.Printf("bytes: %q\n", bytes)
+func SplitWord(bytes []byte) []string {
 	x := gojieba.NewJieba()
 	defer x.Free()
 	var words []string
 	words = x.CutAll(string(bytes))
-	fmt.Println("全模式:", strings.Join(words, "/"))
-
+	//fmt.Println("全模式:", strings.Join(words, "/"))
+	//fmt.Printf("font size %d", len(words))
+	return words
 }
 
-func Frequency() {
-	var s string
-	var words []string
-	use_hmm := true
-	x := gojieba.NewJieba()
-	defer x.Free()
-
-	s = "我来到北京清华大学"
-	words = x.CutAll(s)
-	fmt.Println(s)
-	fmt.Println("全模式:", strings.Join(words, "/"))
-
-	words = x.Cut(s, use_hmm)
-	fmt.Println(s)
-	fmt.Println("精确模式:", strings.Join(words, "/"))
-	s = "比特币"
-	words = x.Cut(s, use_hmm)
-	fmt.Println(s)
-	fmt.Println("精确模式:", strings.Join(words, "/"))
-
-	x.AddWord("比特币")
-	s = "比特币"
-	words = x.Cut(s, use_hmm)
-	fmt.Println(s)
-	fmt.Println("添加词典后,精确模式:", strings.Join(words, "/"))
-
-	s = "他来到了网易杭研大厦"
-	words = x.Cut(s, use_hmm)
-	fmt.Println(s)
-	fmt.Println("新词识别:", strings.Join(words, "/"))
-
-	s = "小明硕士毕业于中国科学院计算所，后在日本京都大学深造"
-	words = x.CutForSearch(s, use_hmm)
-	fmt.Println(s)
-	fmt.Println("搜索引擎模式:", strings.Join(words, "/"))
-
-	s = "长春市长春药店"
-	words = x.Tag(s)
-	fmt.Println(s)
-	fmt.Println("词性标注:", strings.Join(words, ","))
-
-	s = "区块链"
-	words = x.Tag(s)
-	fmt.Println(s)
-	fmt.Println("词性标注:", strings.Join(words, ","))
-
-	s = "长江大桥"
-	words = x.CutForSearch(s, !use_hmm)
-	fmt.Println(s)
-	fmt.Println("搜索引擎模式:", strings.Join(words, "/"))
-
-	wordinfos := x.Tokenize(s, gojieba.SearchMode, !use_hmm)
-	fmt.Println(s)
-	fmt.Println("Tokenize:(搜索引擎模式)", wordinfos)
-
-	wordinfos = x.Tokenize(s, gojieba.DefaultMode, !use_hmm)
-	fmt.Println(s)
-	fmt.Println("Tokenize:(默认模式)", wordinfos)
-
-	keywords := x.ExtractWithWeight(s, 5)
-	fmt.Println("Extract:", keywords)
+// 使用struct 灌入map的数据对value进行排序，必须实现sort接口
+func rankByWordCount(wordFrequencies map[string]int) PairList {
+	pl := make(PairList, len(wordFrequencies))
+	i := 0
+	for k, v := range wordFrequencies {
+		pl[i] = Pair{k, v}
+		i++
+	}
+	sort.Sort(sort.Reverse(pl))
+	return pl
 }
+
+type Pair struct {
+	Key   string
+	Value int
+}
+
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 
 func main() {
-	readFile("./doc/1.txt")
+	bytes, err := ReadFile("./doc/gelin.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	words := SplitWord(bytes)
+
+	wordFrequencyMap := make(map[string]int)
+	for _, word := range words {
+		if value, ok := wordFrequencyMap[word]; ok {
+			wordFrequencyMap[word] = value + 1
+		} else {
+			wordFrequencyMap[word] = 1
+		}
+	}
+	pairList := rankByWordCount(wordFrequencyMap)
+	fmt.Print(pairList)
+
 }
